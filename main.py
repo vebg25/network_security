721d29354d6ba3ee66d4543115c44173eafdc2b9
Vaibhav Gupta <vg498660@gmail.com>
2024-10-27 01:28:28 +0530

    commit trainer

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-# from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.components.model_trainer import ModelTrainer
 
 import sys
 
@@ -32,12 +32,12 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
 
-        # logging.info("Model Training artifact created")
+        logging.info("Model Training artifact created")
         
         
         
